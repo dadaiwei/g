0cad186b3be8a12b9450b3ba40bfd323757d72b1
dadaiwei <dadaiwei@corp.netease.com>
2020-04-07 20:59:04 +0800

    ADD：添加g-mind和g-canvas渲染流程图

--- packages/g-canvas/src/shape/base.ts
+++ packages/g-canvas/src/shape/base.ts
@@ -8,7 +8,7 @@ import * as Shape from './index';
 import Group from '../group';
 
 class ShapeBase extends AbstractShape {
-  getDefaultAttrs() {
+  getDefaultAttrs () {
     const attrs = super.getDefaultAttrs();
     // 设置默认值
     return {
@@ -20,11 +20,11 @@ class ShapeBase extends AbstractShape {
     };
   }
 
-  getShapeBase() {
+  getShapeBase () {
     return Shape;
   }
 
-  getGroupBase() {
+  getGroupBase () {
     return Group;
   }
 
@@ -32,11 +32,11 @@ class ShapeBase extends AbstractShape {
    * 一些方法调用会引起画布变化
    * @param {ChangeType} changeType 改变的类型
    */
-  onCanvasChange(changeType: ChangeType) {
+  onCanvasChange (changeType: ChangeType) {
     refreshElement(this, changeType);
   }
 
-  calculateBBox(): BBox {
+  calculateBBox (): BBox {
     const type = this.get('type');
     const lineWidth = this.getHitLineWidth();
     // const attrs = this.attr();
@@ -59,16 +59,16 @@ class ShapeBase extends AbstractShape {
     };
   }
 
-  isFill() {
+  isFill () {
     return !!this.attrs['fill'] || this.isClipShape();
   }
 
-  isStroke() {
+  isStroke () {
     return !!this.attrs['stroke'];
   }
 
   // 同 shape 中的方法重复了
-  _applyClip(context, clip: ShapeBase) {
+  _applyClip (context, clip: ShapeBase) {
     if (clip) {
       context.save();
       // 将 clip 的属性挂载到 context 上
@@ -83,7 +83,7 @@ class ShapeBase extends AbstractShape {
   }
 
   // 绘制图形时需要考虑 region 限制
-  draw(context: CanvasRenderingContext2D, region?: Region) {
+  draw (context: CanvasRenderingContext2D, region?: Region) {
     const clip = this.getClip();
     // 如果指定了区域，当与指定区域相交时，才会触发渲染
     if (region) {
@@ -102,14 +102,14 @@ class ShapeBase extends AbstractShape {
     this._afterDraw();
   }
 
-  _afterDraw() {
+  _afterDraw () {
     // 绘制的时候缓存包围盒
     this.set('cacheCanvasBBox', this.getCanvasBBox());
     // 绘制后消除标记
     this.set('hasChanged', false);
   }
 
-  skipDraw() {
+  skipDraw () {
     this.set('cacheCanvasBBox', null);
     this.set('hasChanged', false);
   }
@@ -118,7 +118,7 @@ class ShapeBase extends AbstractShape {
    * 绘制图形的路径
    * @param {CanvasRenderingContext2D} context 上下文
    */
-  drawPath(context: CanvasRenderingContext2D) {
+  drawPath (context: CanvasRenderingContext2D) {
     this.createPath(context);
     this.strokeAndFill(context);
     this.afterDrawPath(context);
@@ -129,7 +129,7 @@ class ShapeBase extends AbstractShape {
    * 填充图形
    * @param {CanvasRenderingContext2D} context context 上下文
    */
-  fill(context: CanvasRenderingContext2D) {
+  fill (context: CanvasRenderingContext2D) {
     context.fill();
   }
 
@@ -138,12 +138,12 @@ class ShapeBase extends AbstractShape {
    * 绘制图形边框
    * @param {CanvasRenderingContext2D} context context 上下文
    */
-  stroke(context: CanvasRenderingContext2D) {
+  stroke (context: CanvasRenderingContext2D) {
     context.stroke();
   }
 
   // 绘制或者填充
-  strokeAndFill(context) {
+  strokeAndFill (context) {
     const { lineWidth, opacity, strokeOpacity, fillOpacity } = this.attrs;
 
     if (this.isFill()) {
@@ -172,15 +172,15 @@ class ShapeBase extends AbstractShape {
    * 绘制图形的路径
    * @param {CanvasRenderingContext2D} context 上下文
    */
-  createPath(context: CanvasRenderingContext2D) {}
+  createPath (context: CanvasRenderingContext2D) { }
 
   /**
    * 绘制完成 path 后的操作
    * @param {CanvasRenderingContext2D} context 上下文
    */
-  afterDrawPath(context: CanvasRenderingContext2D) {}
+  afterDrawPath (context: CanvasRenderingContext2D) { }
 
-  isInShape(refX: number, refY: number): boolean {
+  isInShape (refX: number, refY: number): boolean {
     // return HitUtil.isHitShape(this, refX, refY);
     const isStroke = this.isStroke();
     const isFill = this.isFill();
@@ -189,7 +189,7 @@ class ShapeBase extends AbstractShape {
   }
 
   // 之所以不拆成 isInStroke 和 isInPath 在于两者存在一些共同的计算
-  isInStrokeOrPath(x, y, isStroke, isFill, lineWidth) {
+  isInStrokeOrPath (x, y, isStroke, isFill, lineWidth) {
     return false;
   }
 
@@ -197,7 +197,7 @@ class ShapeBase extends AbstractShape {
    * 获取线拾取的宽度
    * @returns {number} 线的拾取宽度
    */
-  getHitLineWidth() {
+  getHitLineWidth () {
     if (!this.isStroke()) {
       return 0;
     }
